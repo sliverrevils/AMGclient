f725173bde6853aceb7672dfd2386744f96985e8
Safonkin Nikita <sliverfrontend@yandex.ru>
2024-03-28 18:41:28 +0300

    03.28.24 new ip

--- app/axiosClient.ts
+++ app/axiosClient.ts
@@ -1,26 +1,26 @@
-import axios from "axios";
-import {toast} from 'react-toastify'
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const axiosClient = axios.create({
-    withCredentials:true,
-    headers:{"secret-key":'any_secret_string'},
-    baseURL:`${process.env.NEXT_PUBLIC_SERVER_URL_DEV1}`,   
-})
+    withCredentials: true,
+    headers: { 'secret-key': 'any_secret_string' },
+    baseURL: `${process.env.NEXT_PUBLIC_SERVER_URL_DEV2}`,
+});
 
-export const axiosError=(err)=>{
-    if(err?.response?.data?.message){   
-        err?.response?.data?.statusCode==403&&console.log('403!!!');
+export const axiosError = (err) => {
+    if (err?.response?.data?.message) {
+        err?.response?.data?.statusCode == 403 && console.log('403!!!');
         toast.error(err.response.data.message, {
-            position: "top-right",
+            position: 'top-right',
             autoClose: 3000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
             draggable: true,
             progress: undefined,
-            theme: "light",
-            });
+            theme: 'light',
+        });
     }
-}
+};
 
 export default axiosClient;
