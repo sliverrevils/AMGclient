6916491052877ba8a1f58ec469d6b5a9e36d9b32
Safonkin Nikita <sliverregulyar@gmail.com>
2024-05-25 09:48:33 +0300

    stat load fix

--- components/Screens/Main/MainScreen.tsx
+++ components/Screens/Main/MainScreen.tsx
@@ -3,9 +3,12 @@ import OrgFlowScreen from "../OrgFlowScreen/OrgFlowScreen";
 import styles from "./main.module.scss";
 import { PieChart } from "@/components/elements/Chart/PieChart";
 import CreateRaport2 from "@/components/modules/ReportTables/CreateRaport2/CreateRaport2";
+import { useSelector } from "react-redux";
+import { StateReduxI } from "@/redux/store";
 
 export default function MainScreen() {
     // const [showOrg, setShowOrg] = useState(false);
+    const { tablePatterns } = useSelector((state: StateReduxI) => state.patterns);
     return (
         <div className={styles.mainWrap}>
             {/* <h1 style={{ margin: 20, opacity: 0.2, textAlign: "center" }}> AMG avtomarket statistics</h1> */}
@@ -13,7 +16,7 @@ export default function MainScreen() {
                 Полная ОРГ-схема
             </div>
             {showOrg && <OrgFlowScreen closeFn={() => setShowOrg(false)} />} */}
-            <CreateRaport2 getPie />
+            {!!tablePatterns.length && <CreateRaport2 getPie />}
         </div>
     );
 }
