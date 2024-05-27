88494fb0f489761537a024720c053aeae705ca0c
Safonkin Nikita <sliverregulyar@gmail.com>
2024-05-27 09:40:54 +0300

    main pie for admin

--- components/Screens/Main/MainScreen.tsx
+++ components/Screens/Main/MainScreen.tsx
@@ -3,14 +3,10 @@ import styles from "./main.module.scss";
 import CreateRaport2 from "@/components/modules/ReportTables/CreateRaport2/CreateRaport2";
 import { useSelector } from "react-redux";
 import { StateReduxI } from "@/redux/store";
+import { UserFullI } from "@/types/types";
 
 export default function MainScreen() {
     const { tablePatterns } = useSelector((state: StateReduxI) => state.patterns);
-    return (
-        <div className={styles.mainWrap}>
-            {
-                //!!tablePatterns.length && <CreateRaport2 getPie />
-            }
-        </div>
-    );
+    const isAdmin = useSelector((state: any) => state.main.user.role === "admin");
+    return <div className={styles.mainWrap}>{isAdmin && !!tablePatterns.length && <CreateRaport2 getPie />}</div>;
 }
